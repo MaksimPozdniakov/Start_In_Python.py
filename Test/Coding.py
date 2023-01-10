bad22525b307b09f5a62fd02779c63ef6ddf55ad
Maksim <p.m.pavlovich@ya.ru>
2023-01-10 18:04:15 +0300

    made corrections

--- Test/Coding.py
+++ Test/Coding.py
@@ -6,7 +6,7 @@ def decode(equation: dict) -> str:
     for key, value in equation.items():
         if value != 0:
             new_equation.append(f'{value}*x**{key}')
-    new_equation = ' + '.join(new_equation) + ' =0'
+    new_equation = ' + '.join(new_equation) + ' = 0'
     new_equation = new_equation.replace('+ -', '- ')\
         .replace(' 1*x', '  x').replace('*x**0', '').replace('x**1', 'x')
     return new_equation
--- Test/Task_01.py
+++ Test/Task_01.py
@@ -1,8 +1,21 @@
 from c_task_01 import create_equation
 from Coding import decode
+from encoding import encode
+from addition import addition
 
 if __name__ == '__main__':
-    equation = create_equation()
-    print(equation)
-    new_equation = decode(equation)
-    print(new_equation)
+    # equation = create_equation()
+    # print(equation)
+    # str_equation = decode(equation)
+    # print(str_equation)
+    # dict_equation = encode(str_equation)
+    # print(dict_equation)
+
+    equation1 = create_equation()
+    equation2 = create_equation()
+    print(decode(equation1))
+    print(decode(equation2))
+    print(decode(addition(equation1, equation2)))
+
+
+
